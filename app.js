80e12017559722bfbacf7417a0643ba510980790
Deepshikha <shikha8835@gmail.com>
2021-02-21 18:05:13 +0530

    completed

--- app.js
+++ app.js
@@ -1,36 +1,96 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
  const app =express();
-var items =["Wake up"];
+
 
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
- app.get("/", function(req, res){
+mongoose.connect("mongodb+srv://Admin_deepshikha:shikha123@cluster0.oaxax.mongodb.net/todolistDB" , {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your To-Do List!"
+});
+
+const item2 = new Item({
+    name: "Hit '+' to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Select this to delete item."
+});
+
+const defaultItems =[item1, item2, item3];
+
+
+app.get("/", function(req, res){
     
-    var today=new Date();
+ 
 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    Item.find({}, function(err, foundItems){
 
-    var day = today.toLocaleDateString("en-US", options);
+        if(foundItems.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err)
+                {
+                    console.log(err);
+                }
+                else{
+                    console.log("Items added successfully.");
+                }
+            });
+            res.redirect("/");
+        }
+        else
+        {
+            res.render("list", {
+                kindofday: "Today" , newlistitem: foundItems
+            });
+        }
 
-    res.render("list", {
-        kindofday: day , newlistitem: items
+       
     });
 
+      
+
     app.post("/", function(req,res){
-        var item = req.body.newitem;
-        items.push(item);
+        
+        const itemName = req.body.newitem;
+
+        const item = new Item({
+            name: itemName
+        });
+
+        item.save();
+
         res.redirect("/");
     });
 
- });
+    app.post("/delete", function(req,res){
+
+        const checkedItemId = req.body.checkbox;
+
+        Item.findByIdAndRemove(checkedItemId, function(err){
+            if(!err)
+            {
+                console.log("Removal Successful!");
+                res.redirect("/");
+            }
+        });
+
+    });
+
+});
 
  app.listen(3000, function(){
      console.log("server is running");
